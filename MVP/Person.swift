78f80f9ca1cda5c4f966519145b3bd1d9651f3c6
komplektan <54449303+komplektan@users.noreply.github.com>
2022-04-29 12:03:53 +0500

    MVP Example

---
+++ MVP/Person.swift
@@ -0,0 +1,13 @@
+//
+//  Person.swift
+//  MVP
+//
+//  Created by Вячеслав Квашнин on 29.04.2022.
+//
+
+import Foundation
+
+struct Person {
+    let name: String
+    let surname: String
+}
--- MVP/ViewController.swift
+++ MVP/ViewController.swift
@@ -11,12 +11,14 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var greetingLabel: UILabel!
     
+    private var person: Person!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        person = Person(name: "Tim", surname: "Cook")
     }
     @IBAction func pressedButton(_ sender: UIButton) {
-        
+        greetingLabel.text = "Hello \(person.name) \(person.surname)!"
     }
 }
 
